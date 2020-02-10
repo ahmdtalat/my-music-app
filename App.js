5f7e506a3b13ea9e74db75c5f1b1e683a9034cbe
ahmdtalaat <ahmd.talat95@gmail.com>
2020-02-10 23:07:13 +0200

    added fonts & header

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const getFonts = () =>
+	Font.loadAsync({
+		'lora-regular': require('./assets/fonts/lora-regular.ttf'),
+		'lora-italic': require('./assets/fonts/lora-italic.ttf')
+	});
+
+import Header from './components/header';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [fontLoaded, setFontLoaded] = useState(false);
+
+	if (fontLoaded) {
+		return (
+			<View>
+				<Header />
+				<View style={styles.container}></View>
+			</View>
+		);
+	} else {
+		return (
+			<AppLoading startAsync={getFonts} onFinish={() => setFontLoaded(true)} />
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff'
+	}
 });
---
+++ components/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const Header = () => {
+	return (
+		<View style={s.header}>
+			<Text style={s.headerText}>My Music</Text>
+		</View>
+	);
+};
+
+const s = StyleSheet.create({
+	header: {
+		height: 70,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#333'
+	},
+	headerText: {
+		marginTop: 20,
+		color: '#eee',
+		fontSize: 22,
+		letterSpacing: 1.2,
+		fontFamily: 'lora-italic'
+	}
+});
+
+export default Header;
